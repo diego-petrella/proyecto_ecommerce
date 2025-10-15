053f7bd89260b0e54662e3324399ba2c0d6679cf
Guillermo Casanova <guillermoluisc@gmail.com>
2025-10-15 20:45:46 -0300

    borrado

--- index.php
+++ index.php
@@ -3,9 +3,7 @@ session_start();
 
 define('APP_INCLUDED', true);
 
-// 1. VERIFICACIÓN DE ADMINISTRADOR LOGUEADO
 if (isset($_SESSION['usuario_id']) && ($_SESSION['usuario_rol_id'] == 1)) {
-    // Redirige al administrador a su panel
     header("Location: /programacion2/articulos/views/panel_admin.php");
     exit;
 }
