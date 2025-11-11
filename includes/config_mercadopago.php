34c461ba5122d616feafc2969229573416ef182b
Diego <145128046+diego-petrella@users.noreply.github.com>
2025-11-11 11:14:25 -0300

    Update config_mercadopago.php
    BORRE TOKEN MP

--- includes/config_mercadopago.php
+++ includes/config_mercadopago.php
@@ -4,7 +4,7 @@
 // Para pruebas, usa las credenciales de 'Test'.
 
 // Access Token: Clave de acceso para la API de Mercado Pago
-define('MP_ACCESS_TOKEN', 'APP_USR-1225567301912849-102209-92de25a83c2180b1cfe2319f888f55ef-1803836373');
+define('MP_ACCESS_TOKEN', '');
 
 // URL base de la aplicación (Necesaria para notificaciones)
 // Debe coincidir con la BASE_URL que definiste en index.php
